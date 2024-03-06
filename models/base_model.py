1b79b7a0cf7b4a5befe298253dca138d2315ef3a
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-03-06 15:32:47 -0800

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,33 @@
-'''Base model file'''
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """String representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dict_rep = self.__dict__.copy()
+        dict_rep["__class__"] = self.__class__.__name__
+        dict_rep["created_at"] = self.created_at.isoformat()
+        dict_rep["updated_at"] = self.updated_at.isoformat()
+        return dict_rep
+
