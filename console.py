9a8ff54b9b3df6ebdecdd65d036590df84b19356
Ahmedihab09 <88886957+Ahmedihab09@users.noreply.github.com>
2024-03-11 01:06:33 +0200

    console

--- console.py
+++ console.py
@@ -2,10 +2,15 @@
 """Entry point of the command interpreter"""
 
 import cmd
-from models import storage
+import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
-
+from models import storage
 class HBNBCommand(cmd.Cmd):
     """Defines the Command Interpreter."""
 
