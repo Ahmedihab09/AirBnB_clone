130d78891e70720a06e31ea1d5ec060c964a57ae
Ahmedihab09 <88886957+Ahmedihab09@users.noreply.github.com>
2024-03-10 02:25:05 +0200

    console

--- console.py
+++ console.py
@@ -2,14 +2,7 @@
 """Entry point of the command interpreter"""
 
 import cmd
-import models
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+
 from models import storage
 
 
