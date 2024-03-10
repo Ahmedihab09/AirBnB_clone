89b942fc3842cce953b98db730395e6eb5bdd538
Ahmedihab09 <88886957+Ahmedihab09@users.noreply.github.com>
2024-03-10 02:24:23 +0200

    console

--- console.py
+++ console.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""Command interpreter module"""
+"""Entry point of the command interpreter"""
 
 import cmd
-import os
 import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
+
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter class"""
+    """Defines the Command Interpreter."""
 
-    file_path = "file.json"
-    prompt = '(hbnb) '
-
-    classes = ["BaseModel"]
-
-    def __init__(self):
-        super().__init__()
-        self.file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file_path)
-
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    prompt = "(hbnb)"
+    classes = ["Amenity", "BaseModel", "City",
+               "Place", "Review", "State", "User"]
 
     def do_EOF(self, arg):
-        """Exit the program"""
-        print()
+        """ EOF command to exit the program"""
+        return True
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program"""
         return True
 
     def emptyline(self):
-        """Do nothing on empty input line"""
+        """ Empty line - Not Excute anything"""
         pass
 
     def do_create(self, arg):
@@ -38,14 +39,61 @@ class HBNBCommand(cmd.Cmd):
         elif arg not in HBNBCommand.classes:
             print("** class doesn't exist **")
         else:
-            print("Available classes:", globals())
-            class_to_create = globals().get(arg)
-            if class_to_create:
-                new_instance = class_to_create()
-                new_instance.save()
-                print(new_instance.id)
-            else:
-                print(f"Class '{arg}' not found in globals()")
+            print(eval(arg)().id)
+            storage.save()
+
+    def do_show(self, arg):
+        """ Prints the string representation of an instance
+        based on the class name and id """
+        a = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif a[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(a) < 2:
+            print("** instance id missing **")
+        elif "{}.{}".format(a[0], a[1]) not in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()["{}.{}".format(a[0], a[1])])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        a = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif a[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(a) < 2:
+            print("** instance id missing **")
+        elif "{}.{}".format(a[0], a[1]) not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()["{}.{}".format(a[0], a[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+        a = arg.split()
+        if arg and a[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            li = []
+            for v in storage.all().values():
+                if not arg or a[0] == v.__class__.__name__:
+                    li.append(str(v))
+            print(li)
+
+    def do_count(self, arg):
+        """retrieve the number of instances of a class"""
+        count = 0
+        a = arg.split()
+        for v in storage.all().values():
+            if a[0] == v.__class__.__name__:
+                count += 1
+        print(count)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
