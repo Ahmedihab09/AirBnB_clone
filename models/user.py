3694fe25405faa6444b18ad8898ba48c56c1846f
Mohammad_Adel <mohammadadel8727@gmail.com>
2024-03-11 03:31:39 +0200

    added newLine at the end of files

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel."""
-    email =""
+    email = ""
     password = ""
     first_name = ""
     last_name = ""
