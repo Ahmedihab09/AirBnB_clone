fd0aa17653b748b7814999b9db935b43291c2371
Mohammad_Adel <mohammadadel8727@gmail.com>
2024-03-11 02:29:50 +0200

    more and more testsexit

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,6 +3,7 @@
 '''Test state class'''
 import unittest
 from models.state import State
+from tests.test_models import test_base_model
 
 class TestState(unittest.TestCase):
     def test_default_attributes(self):
